7b48e4c329d2bc95d4e57e895a19ead35c6f39f2
Jackson Marcondes <jackson.adriano94@gmail.com>
2020-11-14 03:24:51 -0300

    Adicionado views e rotas iniciais

---
+++ resources/views/welcomeUser.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="{{asset ('assets/css/materialize.min.css')}}" media="screen,projection">
+    <link type="text/css" rel="stylesheet" href="{{asset ('css/app.css')}}" media="screen,projection">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+<body>
+<div class="valign-wrapper row">
+
+    <div class="col s12">
+        <div class="card blue-grey lighten-5 hoverable">
+            <div class="card-content white-text">
+                <span class="card-title black-text">Títulos disponíveis para retirada</span>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card blue-grey darken-3">
+                            <div class="card-content white-text">
+                                <span class="card-title">Card Title</span>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <span>Autor: Jackson</span>
+                                <span class="right"><button class="btn btn-small">RESERVAR</button></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="{{asset ('js/app.js')}}"></script>
+<script type="text/javascript" src="{{asset ('assets/js/materialize.min.js')}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('welcomeUser');
+});
+
+Route::get('/homeAdmin', function () {
+    return view('welcomeAdmin');
+});
