a82b8bdcc52d93690c60d76eef43198e6216d91e
Jackson Marcondes <jackson.adriano94@gmail.com>
2020-11-28 20:09:17 -0300

    Alterado titulo das telas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>SGB</title>
 
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
